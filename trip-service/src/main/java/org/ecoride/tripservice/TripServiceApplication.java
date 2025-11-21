e033782062ff63ec8d85bff1bc862215bdf6439c
Mateo RD <tu-correo@example.com>
2025-11-20 23:47:35 -0500

    agreago paiment service

--- trip-service/src/main/java/org/ecoride/tripservice/TripServiceApplication.java
+++ trip-service/src/main/java/org/ecoride/tripservice/TripServiceApplication.java
@@ -2,6 +2,8 @@ package org.ecoride.tripservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
